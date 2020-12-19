4c831ad31f1b3de74603199fd1fb299cfbf0d25a
Patrick Carlson <carlson2442@gmail.com>
2020-12-19 16:07:49 -0700

    start migrating to github actions

--- deploy.R
+++ deploy.R
@@ -1,9 +1,9 @@
 library(rsconnect)
 
 # Set the account info for deployment.
-setAccountInfo(name   = Sys.getenv("shinyapps_name"),
-               token  = Sys.getenv("shinyapps_token"),
-               secret = Sys.getenv("shinyapps_secret"))
+setAccountInfo(name   = Sys.getenv("SHINYAPPS_NAME"),
+               token  = Sys.getenv("SHINYAPPS_TOKEN"),
+               secret = Sys.getenv("SHINYAPPS_SECRET"))
 
 # Deploy the application.
 deployApp()
