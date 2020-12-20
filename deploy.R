c9a5abb3e955343798d6bcaba068de13e00bdfe4
Patrick Carlson <carlson2442@gmail.com>
2020-12-19 17:34:35 -0700

    move deployment to commandline, not R script

--- deploy.R
+++
@@ -1,9 +0,0 @@
-library(rsconnect)
-
-# Set the account info for deployment.
-setAccountInfo(name   = Sys.getenv("SHINYAPPS_NAME"),
-               token  = Sys.getenv("SHINYAPPS_TOKEN"),
-               secret = Sys.getenv("SHINYAPPS_SECRET"))
-
-# Deploy the application.
-deployApp()
